58356b4d0db94e9daf983e4ab8fd98bb399ff4ea
Mars Geldard <github@mail.themartianlife.com>
2019-06-19 17:12:38 +1000

    Started DrawingDetector Mars

---
+++ Chapter14-DrawingDetector/DDDemo/DDDemo/Category.swift
@@ -0,0 +1,11 @@
+//
+//  Category.swift
+//  DDDemo
+//
+//  Created by Mars Geldard on 19/6/19.
+//  Copyright © 2019 Mars Geldard. All rights reserved.
+//
+
+enum Category: String {
+    case thing
+}
