69fb09cdc037c3fecc7ff642d5ba9123a4cc8014
Mars Geldard <github@mail.themartianlife.com>
2019-06-27 10:22:46 +1000

    README update

--- Chapter17-StillImageAnalysis/Snippets/Snippets.playground/Contents.swift
+++ Chapter17-StillImageAnalysis/Snippets/Snippets.playground/Contents.swift
@@ -66,12 +66,47 @@ extension UIImage {
         }
     }
     
-    func cropWithSaliency(to size: CGSize, prioritising saliencyType: SaliencyType = .attentionBased) -> UIImage? {
+    func croppedWithSaliency(to size: CGSize, prioritising saliencyType: SaliencyType = .attentionBased) -> UIImage? {
         if self.size.width < size.width || self.size.height < size.height { return nil }
+        guard let cgImage = self.cgImage, let handler = VNImageRequestHandler(uiImage: self) else {
+            return self
+        }
+        
+        let request = saliencyType.request
+        request.queueFor(image: self) { results in
+            if let result = request.results?.first as? VNSaliencyImageObservation {
+                
+            }
+        }
+        
+        //var unionOfSalientRegions = CGRect(x: 0, y: 0, width: 0, height: 0)
+        //let errorPointer = NSErrorPointer(nilLiteral: ())
+        //let salientObjects = saliencyObservation.salientObjectsAndReturnError(errorPointer)
+        //for salientObject in salientObjects {
+        //    unionOfSalientRegions = unionOfSalientRegions.union(salientObject.boundingBox)
+        //}
+        //self.salientRect = VNImageRectForNormalizedRect(unionOfSalientRegions,
+        //                                                originalImage.extent.size.width,
+        //                                                originalImage.extent.size.height)
+        //
+        //public func createHeatMapMask(from observation: VNSaliencyImageObservation) -> CGImage? {
+        //    let pixelBuffer = observation.pixelBuffer
+        //    let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
+        //    let vector = CIVector(x: 0, y: 0, z: 0, w: 1)
+        //    let saliencyImage = ciImage.applyingFilter("CIColorMatrix", parameters: ["inputBVector": vector])
+        //    return CIContext().createCGImage(saliencyImage, from: saliencyImage.extent)
+        //}
+        
         
         // TODO
+        // get salientRect, modify to Size
         
-        return self
+        DispatchQueue.global(qos: .userInitiated).async {
+            let croppedImage = ciImage.cropped(to: salientRect)
+            return UIImage(ciImage: croppedImage)
+        }
+        
+        return nil
     }
     
     var cgImageOrientation: CGImagePropertyOrientation {
@@ -89,10 +124,15 @@ extension UIImage {
 }
 
 // we included a test image in the playground Resources
-let testImage = UIImage(named: "test.jpg")!
+let barcodeTestImage = UIImage(named: "test.jpg")!
 
-testImage.detectBarcodes { barcodes in
+barcodeTestImage.detectBarcodes { barcodes in
     for barcode in barcodes {
         print("Barcode data: \(barcode.payloadStringValue ?? "None")")
     }
 }
+
+let saliencyTestImage = UIImage(named: "test2.jpg")!
+let thumbnailSize = CGSize(width: 400, height: 400)
+let attentionRegionImage = saliencyTestImage.croppedWithSaliency(to: thumbnailSize, prioritising: .attentionBased)
+let objectsRegionImage = saliencyTestImage.croppedWithSaliency(to: thumbnailSize, prioritising: .objectnessBased)
