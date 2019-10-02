6919aed370a75f04b3809e3b5ffc5271e94fbec6
Jon Manning <jon@secretlab.com.au>
2019-10-02 14:53:39 +1000

    Fix launching to black screen in ISDemo and SRDemo; add NSCameraUsageDescription to ISDemo projects

--- Chapter 4 - Vision/Image Similarity/ISDemo-Complete/ISDemo/AppDelegate.swift
+++ Chapter 4 - Vision/Image Similarity/ISDemo-Complete/ISDemo/AppDelegate.swift
@@ -28,7 +28,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        let window = UIWindow(frame: UIScreen.main.bounds)
+        let window = UIWindow(windowScene: scene as! UIWindowScene)
         window.rootViewController = UIHostingController(rootView: ContentView())
         self.window = window
         window.makeKeyAndVisible()
